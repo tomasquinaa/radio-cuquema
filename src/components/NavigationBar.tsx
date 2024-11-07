c4ad3f3352563a2b04258f0e2bce208837b31295
tomasquinaa <armando.quinanga2023@gmail.com>
2024-11-07 11:28:54 +0100

    feat: atualizado novamente

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import { IoIosHome } from 'react-icons/io';
+import React from "react";
+import { IoIosHome } from "react-icons/io";
+import { IoBriefcase, IoPlayCircle, IoRecording } from "react-icons/io5";
 
 const NavigationBar: React.FC = () => (
   <nav className="bg-primaryPurple text-white">
-    <div className="flex justify-center items-center space-x-8 p-4 mr-[40rem]">
-      <a href="#" className="hover:underline flex items-center">
-        <IoIosHome className="bg-yellow-500" />
+    <div className="flex justify-center items-center space-x-8 p-4">
+      <a href="#" className="flex items-center space-x-2 hover:underline">
+        <IoIosHome size={24} className="bg-yellow-500 p-1 rounded-full" />
+        <span>Início</span>
       </a>
-      <a href="https://podcasters.spotify.com/pod/show/radiocuquema">Programas Gravados</a>
-      <a href="#">Empregos</a>
-      <a href="#">Emissão em Direto</a>
+      <a
+        href="https://podcasters.spotify.com/pod/show/radiocuquema"
+        className="flex items-center space-x-2 hover:underline"
+      >
+        <IoRecording size={24} />
+        <span>Programas Gravados</span>
+      </a>
+      <a href="#" className="flex items-center space-x-2 hover:underline">
+        <IoBriefcase size={24} />
+        <span>Empregos</span>
+      </a>
+      <a href="#" className="flex items-center space-x-2 hover:underline">
+        <IoPlayCircle size={24} />
+        <span>Emissão em Direto</span>
+      </a>
     </div>
   </nav>
 );
