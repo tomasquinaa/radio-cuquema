38b0e79542a8aaedc3eb97551070a40deabda36e
tomasquinaa <armando.quinanga2023@gmail.com>
2024-11-15 09:20:51 +0100

    feat: atualizado o site

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,29 +1,55 @@
 import React from "react";
 import { IoIosHome } from "react-icons/io";
-import { IoBriefcase, IoPlayCircle, IoRecording } from "react-icons/io5";
+import { IoBriefcase, IoCodeSlash, IoMic, IoPlayCircle, IoRecording } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const NavigationBar: React.FC = () => (
   <nav className="bg-primaryPurple text-white">
     <div className="flex justify-center items-center space-x-8 p-4">
-      <a href="#" className="flex items-center space-x-2 hover:underline">
+      <Link to="/" className="flex items-center space-x-2 hover:underline">
         <IoIosHome size={24} className="bg-yellow-500 p-1 rounded-full" />
         <span>Início</span>
-      </a>
-      <a
-        href="https://podcasters.spotify.com/pod/show/radiocuquema"
+      </Link>
+      <Link
+        to=""
+        className="flex items-center space-x-2 hover:underline"
+      >
+        <IoPlayCircle size={24} />
+        <span>Play</span>
+      </Link>
+      <Link
+        to=""
+        className="flex items-center space-x-2 hover:underline"
+      >
+        <IoCodeSlash size={24} />
+        <span>Programação</span>
+      </Link>
+      <Link
+        to=""
+        className="flex items-center space-x-2 hover:underline"
+      >
+        <IoMic size={24} />
+        <span>Podcast</span>
+      </Link>
+      <Link
+        to=""
+        className="flex items-center space-x-2 hover:underline"
+      >
+        <IoBriefcase size={24} />
+        <span>Empregos</span>
+      </Link>
+      <Link
+        to="https://podcasters.spotify.com/pod/show/radiocuquema"
         className="flex items-center space-x-2 hover:underline"
       >
         <IoRecording size={24} />
         <span>Programas Gravados</span>
-      </a>
-      <a href="#" className="flex items-center space-x-2 hover:underline">
+      </Link>
+      <Link to="#" className="flex items-center space-x-2 hover:underline">
         <IoBriefcase size={24} />
-        <span>Empregos</span>
-      </a>
-      <a href="#" className="flex items-center space-x-2 hover:underline">
-        <IoPlayCircle size={24} />
-        <span>Emissão em Direto</span>
-      </a>
+        <span>Publicidades</span>
+      </Link>
+  
     </div>
   </nav>
 );
